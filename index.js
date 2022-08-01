1c749e44efe0da5ba655f57b30df2f364b578dc0
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-01 18:07:00 +0600

    array element set element index search from index to search value done

---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [34, 54, 65, 56, 90];
+
+// 1.get element value by index
+var element = numbers[1];
+// console.log(element);
+// 2.set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+// 3. find index of an element
+var positionIndex = numbers.indexOf(34);
+console.log(positionIndex);
