0232807a9097f9899f8668e76cf3aaac84a17c66
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-01 15:50:54 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,32 @@
+// Number Type Variable
+var sunglassPrice = 120;
+
+//String Type Variable
+var specialOneName = 'Ariana Grande';
+
+// boolean Type Variable
+var isSingle = true;
+
+// Two Types Of Number: Integer:120,
+// Float, Decimal: 3.65,
+
+
+// Variable Naming Convention: Rules, Best Practice
+/**
+ * 1. Single Word
+ * 2. No Gap or space
+ * 3. no quote
+ * 4. no keyword
+ * 5. can't start with a number
+ * 6. can't contain dash
+ * 7. should use camelCase
+ */
+
+// Math Operations: +, -, *, /
+// Shorthand: +=, -=, *=, /=, ++, --
+
+// Convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder: Vagsesh : %
