3d1c3ab50e0bb3751ce83cbafc4b5f5d987508b5
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-01 19:29:17 +0600

    push and pop done

---
+++ push-pop.js
@@ -0,0 +1,22 @@
+var numbers = [78, 45, 98, 45];
+// Use Push to add element in an array as the last element of the array
+// numbers.push(63);
+// console.log(numbers);
+var friends = ['balam', 'kalam', 'salam'];
+// friends.push('gelam');
+// friends.push('pailam');
+friends.unshift('mamun');
+friends.unshift('jamil');
+console.log(friends);
+var remove = friends.shift();
+console.log(friends);
+console.log(remove);
+
+
+// use pop to get rid of the last element
+// console.log(numbers);
+// numbers.pop();
+// var element = numbers.pop();
+// console.log(numbers);
+// console.log(element);
+
